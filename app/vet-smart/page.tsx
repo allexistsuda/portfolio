9cd8c587dbaf45745fcf0dba5c6ee57d8f6cb9a5
Allexis Tsuda <allexistsuda@Allexiss-MacBook-Air.local>
2026-05-18 09:12:06 -0300

    padronizar espaçamento rodapé em todas as internals

--- app/vet-smart/page.tsx
+++ app/vet-smart/page.tsx
@@ -248,7 +248,7 @@ export default function VetSmart() {
           </div>
           <div className="hidden md:block"><Img src="/vet-smart/07.gif" alt="Vet Smart color" /></div>
         </div>
-        <div style={{ marginBottom: 100 }}>
+        <div>
           <div className="md:hidden space-y-8">
             <Img src="/vet-smart/08-mobile-a.gif" alt="Vet Smart" />
             <Img src="/vet-smart/08-mobile-b.png" alt="Vet Smart" />
